c661c0edfef2378b8bcba0323d37997734c34453
Administrator <Administrator@DESKTOP-89344TI>
2019-03-02 10:32:18 +0600

    Prome Foods Automation by RAWN Technologies Ltd. Device Integration

--- src/mssccesstopostgres_prome/cardupdatefrompos.java
+++ src/mssccesstopostgres_prome/cardupdatefrompos.java
@@ -25,45 +25,44 @@ public class cardupdatefrompos {
        Connection ca = null;
        Statement stmta = null;
        String maxTIme = "2019-02-01 00:00:00.0000";
+       String device = null;
        int hunits = 0;
-        for (int devicesl =1; devicesl < 6; devicesl++) {
+      //  for (int devicesl =1; devicesl < 200; devicesl++) {
        try {
        Class.forName("org.postgresql.Driver");
          cp = DriverManager
             .getConnection("jdbc:postgresql://localhost:5433/pos2019",
-            "postgres", "syspass");
+            "postgres", "syspassrawn");
          cp.setAutoCommit(false);
          System.out.println("Opened  Postgress database successfully");
          stmtp = cp.createStatement();
-         ResultSet rs = stmtp.executeQuery( "select maxtime, tlu.units from (select  max(r.datenew) maxtime  from receipts r, tickets t, ticketlines tl, products p\n" +
+         ResultSet rs = stmtp.executeQuery( "select maxtime, tlu.units , device  from (select  max(r.datenew) maxtime, p.reference device  from receipts r, tickets t, ticketlines tl, products p\n" +
 "where r.id=t.id\n" +
 "and t.id=tl.ticket\n" +
-"and tl.product=p.id and  p.reference ='" + devicesl +"' "
+"and tl.product=p.id group by p.reference "
         + ") d, ticketlines tlu, receipts ru, products pu\n" +
 "where d.maxtime=ru.datenew\n" +
 "and ru.id=tlu.ticket\n" +
 "and tlu.product=pu.id\n" +
-"and pu.reference='" + devicesl +"';" );
+" and d.device=pu.reference " + 
+" and ru.datenew + interval '1 hour' > now();" );
          while ( rs.next() ) {
          maxTIme = rs.getString("maxtime");
+         device = rs.getString("device");
          hunits = rs.getInt("units");
          if( maxTIme == null)
          {
-             maxTIme="2018-06-30 00:00:00.0000";
+             maxTIme="2019-02-05 00:00:00.0000";
              
          }
-         System.out.println(devicesl+ " Max Time " +maxTIme);
-            }
-       }
-        catch (Exception ex) {
-            ex.printStackTrace();
-                }
+         System.out.println(device + " Max Time " +maxTIme);
+           
        
        
        try {
          Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
          ca = DriverManager
-             .getConnection("jdbc:ucanaccess://D:/AccessControl/iCCard3000.mdb",
+             .getConnection("jdbc:ucanaccess://C:/AccessControl/iCCard3000.mdb",
             "", "168168");
          ca.setAutoCommit(false);
          System.out.println("Opened Access database successfully");
@@ -71,7 +70,7 @@ public class cardupdatefrompos {
            
            try (  
                    ResultSet rsa = stmta.executeQuery( "SELECT t_b_Consumer.f_ConsumerID, t_b_Consumer.f_CardNO, t_b_Consumer.f_EndYMD\n" +
-"FROM t_b_Consumer where t_b_Consumer.f_ConsumerID ="+ devicesl+";")) {
+"FROM t_b_Consumer where t_b_Consumer.f_ConsumerID ="+ device+";")) {
                    
  
                while ( rsa.next() ) {
@@ -96,7 +95,7 @@ public class cardupdatefrompos {
                        System.out.println("User " + userNo +" Time "+ checkTime);
 
                        String sql = "update  t_b_Consumer  set  t_b_Consumer.f_EndYMD = '" + maxTIme
-                              + "' where f_ConsumerID =" + devicesl + ";";
+                              + "' where f_ConsumerID =" + device + ";";
                       System.out.println(sql);
                       stmta.executeUpdate(sql);
                        ca.commit();
@@ -106,6 +105,12 @@ public class cardupdatefrompos {
                } }
 
             }
+       
+        catch (Exception ex) {
+            ex.printStackTrace();
+                }
+    }
+   }
         catch (Exception ex) {
             ex.printStackTrace();
                 }finally {
@@ -130,5 +135,4 @@ catch (Exception ex) {
 
     }
     }
-   }
 }
